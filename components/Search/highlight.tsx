5c60b0fae7c85faaf19c67af30050b9375c3bd32
ikoamu <ikoamu@gmail.com>
2024-04-07 19:29:04 +0900

    format

--- components/Search/highlight.tsx
+++ components/Search/highlight.tsx
@@ -1,24 +1,24 @@
-import { RangeTuple } from "fuse.js";
+import { RangeTuple } from 'fuse.js'
 import { Text } from '@chakra-ui/react'
 
 export const highlightMatches = (text: string, indices: readonly RangeTuple[] = []) => {
-  const children: React.ReactNode[] = [];
-  let nextNoMatchedIndex = 0;
+  const children: React.ReactNode[] = []
+  let nextNoMatchedIndex = 0
 
   indices.forEach((idc, i) => {
-    const lastRegionNextIndex = idc[1] + 1;
+    const lastRegionNextIndex = idc[1] + 1
     children.push(
       ...[
         text.substring(nextNoMatchedIndex, idc[0]).replace(' ', '\u00A0'),
         <Text key={idc + ' ' + i} as="u">
           {text.substring(idc[0], lastRegionNextIndex).replace(' ', '\u00A0')}
         </Text>,
-      ]
-    );
-    nextNoMatchedIndex = lastRegionNextIndex;
-  });
+      ],
+    )
+    nextNoMatchedIndex = lastRegionNextIndex
+  })
 
-  children.push(text.substring(nextNoMatchedIndex).replace(' ', '\u00A0'));
+  children.push(text.substring(nextNoMatchedIndex).replace(' ', '\u00A0'))
 
-  return <>{children}</>;
-};
+  return <>{children}</>
+}
--- components/Search/index.tsx
+++ components/Search/index.tsx
@@ -1,21 +1,24 @@
-import { IconButton, Modal, ModalOverlay, Tooltip, useDisclosure } from "@chakra-ui/react";
+import { IconButton, Modal, ModalOverlay, Tooltip, useDisclosure } from '@chakra-ui/react'
 import { SearchIcon } from '@chakra-ui/icons'
 import { NodeObject } from 'force-graph'
-import { SearchContent } from "./SearchContent";
-import { useCallback } from "react";
-import { NodeById } from "../../pages";
+import { SearchContent } from './SearchContent'
+import { useCallback } from 'react'
+import { NodeById } from '../../pages'
 
 export const Search: React.FC<{
-  nodeById: NodeById,
-  setPreviewNode: (newPresent: NodeObject) => void,
+  nodeById: NodeById
+  setPreviewNode: (newPresent: NodeObject) => void
 }> = ({ nodeById, setPreviewNode }) => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { isOpen, onOpen, onClose } = useDisclosure()
 
-  const handleOnClickResultItem = useCallback((id: string) => {
-    const node = nodeById[id];
-    onClose();
-    if (!!node) setPreviewNode(node);
-  }, [nodeById, onClose, setPreviewNode])
+  const handleOnClickResultItem = useCallback(
+    (id: string) => {
+      const node = nodeById[id]
+      onClose()
+      if (!!node) setPreviewNode(node)
+    },
+    [nodeById, onClose, setPreviewNode],
+  )
 
   return (
     <>
@@ -26,15 +29,15 @@ export const Search: React.FC<{
           variant="subtle"
           icon={<SearchIcon />}
           onClick={(e) => {
-            e.currentTarget.blur();
-            onOpen();
+            e.currentTarget.blur()
+            onOpen()
           }}
         />
       </Tooltip>
-      <Modal isOpen={isOpen} onClose={onClose} >
+      <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
         <SearchContent onClickResultItem={handleOnClickResultItem} />
       </Modal>
     </>
-  );
-};
+  )
+}
