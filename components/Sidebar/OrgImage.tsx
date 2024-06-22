009e0f8b1d65b3767edd7cee3fc56a1ff399af5c
Shotaro Aoki <ikoamu@gmail.com>
2024-06-22 16:28:02 +0900

    Revert "Revert "support org-html""

--- components/Sidebar/OrgImage.tsx
+++ components/Sidebar/OrgImage.tsx
@@ -5,24 +5,34 @@ import path from 'path'
 import { Container } from '@chakra-ui/react'
 //import '../../../public/placeholder.png'
 
+const convertToNextImageAttr = (value: string | number | undefined): number | undefined => {
+  if (value === undefined) {
+    return undefined
+  }
+
+  if (typeof value === 'number' || !isNaN(Number(value))) {
+    return Number(value)
+  }
+
+  if (value.endsWith('px')) {
+    return Number(value.replace('px', ''))
+  }
+
+  return undefined
+}
+
 export interface OrgImageProps {
-  src: string
+  org: React.ClassAttributes<HTMLImageElement> & React.ImgHTMLAttributes<HTMLImageElement>
   file: string
 }
 
 export const OrgImage = (props: OrgImageProps) => {
-  const { src, file } = props
+  const { org, file } = props
+  const { src, ...attr } = org
 
-  // const [image, setImage] = useState<any>(null)
-
-  /* )
-*   .then((res) => res.blob())
-*   .then((res) => setImage(res))
-*   .catch((e) => {
-*     setImage(null)
-*     console.error(e)
-*   })
-}, [fullPath]) */
+  if (!src) {
+    return null
+  }
 
   const dumbLoader = ({ src, width, quality }: { [key: string]: string | number }) => {
     return `${src}`
@@ -31,17 +41,38 @@ export const OrgImage = (props: OrgImageProps) => {
     return `img/${src}`
   }
 
-  if (src.startsWith("file:")) {
+  if (src?.startsWith("file:")) {
     const srcName = src.replace(/file:/g, './')
+    const attrWidth = convertToNextImageAttr(attr.width)
+    const attrHeight = convertToNextImageAttr(attr.height)
+    const layout = attrWidth || attrHeight ? undefined : "responsive"
     return (
-      <Image layout="responsive" loader={dumbLoader} src={srcName} alt="" />
+      <Image
+        {...attr}
+        layout={layout}
+        loader={dumbLoader}
+        src={srcName}
+        width={attrWidth}
+        height={attrHeight}
+        alt={attr.alt ?? "Wow, an image."}
+      />
     )
   }
 
   if (/(http)?.*/g.test(src.replace(/(http)?.*/g, '$1'))) {
-    console.log(src.replace(/(http)?.*/g, '$1'))
+    const attrWidth = convertToNextImageAttr(attr.width)
+    const attrHeight = convertToNextImageAttr(attr.height)
+    const layout = attrWidth || attrHeight ? undefined : "responsive"
     return (
-      <Image layout="responsive" loader={dumbLoader} src={src} alt="" />
+      <Image
+        {...attr}
+        layout={layout}
+        loader={dumbLoader}
+        src={src}
+        width={attrWidth}
+        height={attrHeight}
+        alt={attr.alt ?? "Wow, an image."}
+      />
     )
   }
 
@@ -51,10 +82,10 @@ export const OrgImage = (props: OrgImageProps) => {
   const fullPath =
     path.isAbsolute(srcName) || srcName.slice(0, 1) === '~' ? srcName : path.join(dir, srcName)
   const encodedPath = encodeURIComponent(encodeURIComponent(fullPath))
-
+  console.log(srcName, " is ", "local")
   return (
     <Container my={4} position="relative">
-      <img alt="Wow, an image." src={fullPath} />
+      <img {...attr} alt={attr.alt ?? "Wow, an image."} src={fullPath} />
     </Container>
   )
 }
