2ba6791cd2b20cd5845eafe49118c1fe9ec7d6f2
ikoamu <ikoamu@gmail.com>
2024-06-22 15:33:59 +0900

    feat: Refactor Sidebar Section component
    This commit refactors the Sidebar Section component by removing commented out code and console.log statements. It improves the code readability and maintainability.

--- components/Sidebar/Section.tsx
+++ components/Sidebar/Section.tsx
@@ -20,77 +20,83 @@ export const Section = (props: SectionProps) => {
     setOpen(!collapse)
   }, [collapse])
 
+
+
   if (className === 'h0Wrapper headingWrapper') {
     return <Box className="preHeadingContent"> {children}</Box>
   }
-  const kids = children as ReactChild[]
-  return (
-    <Box className={'sec'}>
-      <Box display="block">
-        <Flex className="headingFlex" alignItems="baseline">
-          {open && kids.length > 0 ? (
-            <>
-              <IconButton
-                className="viewerHeadingButton"
-                _focus={{}}
-                _active={{}}
-                aria-label="Expand heading"
-                //mr={1}
-                size="xs"
-                variant="subtle"
-                icon={<ChevronUpIcon />}
-                onClick={() => setOpen(!open)}
-                height={2}
-                width={2}
-              />
-              <IconButton
-                className="outlineHeadingButton"
-                _focus={{}}
-                _active={{}}
-                aria-label="Expand heading"
-                //mr={1}
-                size="xs"
-                variant="subtle"
-                icon={<VscCircleOutline />}
-                onClick={() => setOpen(!open)}
-                height={2}
-                width={2}
-              />
-            </>
-          ) : (
-            <>
-              <IconButton
-                className="viewerHeadingButton"
-                _active={{}}
-                _focus={{}}
-                aria-label="Collapse heading"
-                //mr={1}
-                height={2}
-                width={2}
-                size="xs"
-                variant="subtle"
-                icon={<ChevronDownIcon />}
-                onClick={() => setOpen(!open)}
-              />
-              <IconButton
-                className="outlineHeadingButton"
-                _active={{}}
-                _focus={{}}
-                aria-label="Collapse heading"
-                //mr={1}
-                height={2}
-                width={2}
-                size="xs"
-                variant="subtle"
-                icon={<VscCircleFilled />}
-                onClick={() => setOpen(!open)}
-              />
-            </>
-          )}
-          {kids[0]}
-        </Flex>
-      </Box>
-      {open && <Box className="sectionContent">{kids.slice(1)}</Box>}
-    </Box>
-  )
+  // const kids = children as ReactChild[]
+  // return (
+  //   <Box className={'sec'}>
+  //     <Box display="block">
+  //       <Flex className="headingFlex" alignItems="baseline">
+  //         {open && kids.length > 0 ? (
+  //           <>
+  //             <IconButton
+  //               className="viewerHeadingButton"
+  //               _focus={{}}
+  //               _active={{}}
+  //               aria-label="Expand heading"
+  //               //mr={1}
+  //               size="xs"
+  //               variant="subtle"
+  //               icon={<ChevronUpIcon />}
+  //               onClick={() => setOpen(!open)}
+  //               height={2}
+  //               width={2}
+  //             />
+  //             <IconButton
+  //               className="outlineHeadingButton"
+  //               _focus={{}}
+  //               _active={{}}
+  //               aria-label="Expand heading"
+  //               //mr={1}
+  //               size="xs"
+  //               variant="subtle"
+  //               icon={<VscCircleOutline />}
+  //               onClick={() => setOpen(!open)}
+  //               height={2}
+  //               width={2}
+  //             />
+  //           </>
+  //         ) : (
+  //           <>
+  //             <IconButton
+  //               className="viewerHeadingButton"
+  //               _active={{}}
+  //               _focus={{}}
+  //               aria-label="Collapse heading"
+  //               //mr={1}
+  //               height={2}
+  //               width={2}
+  //               size="xs"
+  //               variant="subtle"
+  //               icon={<ChevronDownIcon />}
+  //               onClick={() => setOpen(!open)}
+  //             />
+  //             <IconButton
+  //               className="outlineHeadingButton"
+  //               _active={{}}
+  //               _focus={{}}
+  //               aria-label="Collapse heading"
+  //               //mr={1}
+  //               height={2}
+  //               width={2}
+  //               size="xs"
+  //               variant="subtle"
+  //               icon={<VscCircleFilled />}
+  //               onClick={() => setOpen(!open)}
+  //             />
+  //           </>
+  //         )}
+  //         {kids[0]}
+  //       </Flex>
+  //     </Box>
+  //     {open && <Box className="sectionContent">{kids.slice(1)}</Box>}
+  //   </Box>
+  // )
+  console.log(children)
+  return <>
+    {JSON.stringify(props)}
+  </>
 }
